fc9fa72e71a61c748118d53b76ccb098eba85c03
pujiyanto <puji.yantov@gmail.com>
2022-09-04 17:28:27 +0700

    remove footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,64 +1,4 @@
 <footer>
-    <div class="footer-wrapper">
-        <div class="footer-area footer-padding">
-            <div class="container">
-                <div class="row justify-content-between">
-                    <div class="col-xl-4 col-lg-3 col-md-8 col-sm-8">
-                        <div class="single-footer-caption mb-50">
-                            <div class="single-footer-caption mb-30">
-                                <div class="footer-logo mb-35">
-                                    <a href="{{ route('home') }}"><img src="{{ asset('/assets/img/logo/logo-kbkm.png') }}"
-                                            class="d-none d-lg-block d-xl-none" alt="" /></a>
-
-                                    {{-- D-MOBILE --}}
-                                    <a href="{{ route('home') }}"><img
-                                            src="{{ asset('/assets/img/logo/logo-kbkm.png') }}" width="100"
-                                            class="d-lg-none d-xl-block" alt="" /></a>
-                                </div>
-                                <div class="footer-tittle">
-                                    <div class="footer-pera">
-                                        <p>
-                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam vero eveniet,
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4">
-                        <div class="single-footer-caption mb-50">
-                            <div class="footer-tittle">
-                                <h4>Menu</h4>
-                                <ul>
-                                    <li><a href="#">Beranda</a></li>
-                                    <li><a href="#">Produk Budaya</a></li>
-                                    <li><a href="#">Travel Budaya</a></li>
-                                    <li><a href="#">Term & Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-8 col-md-8 col-sm-8">
-                        <div class="single-footer-caption mb-50">
-                            <div class="footer-tittle" style="color: #C2C5DB; font-weight: 300; font-size: 14px;">
-                                <h4>INFORMASI</h4>
-                                <ul>
-                                    <li>Direktorat Pembinaan Tenaga dan Lembaga Kebudayaan,
-                                        Direktorat Jenderal Kebudayaan Republik Indonesia</li>
-                                    <li>
-                                        Komplek Kemdikbud, Gedung E Lt.9 <br>
-                                        Jln. Jenderal Sudirman Senayan Jakarta 10270
-                                    </li>
-                                    <li>Telp: (021) 5725540 Fax: (021) 5725539</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="footer-bottom-area">
         <div class="container">
             <div class="footer-border">
@@ -72,7 +12,7 @@
                                 </script>
                                 All rights reserved ||
                                 by
-                                <a href="#" target="_blank">Developer</a>
+                                <a href="#" target="_blank">Kemah Budaya Kaum Muda</a>
                             </p>
                         </div>
                     </div>
