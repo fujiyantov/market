58c16151aaedbfc2f589c57731d3f20f5b39c7d2
pujiyanto <puji.yantov@gmail.com>
2022-09-08 19:40:11 +0700

    remove kontak

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -24,44 +24,17 @@
                                     width="90" alt="" /></a>
                         </div>
 
-                        {{-- <div class="logo2 navbar-brand">
-                            <a href="index.html"><img src="{{ asset('/assets/img/logo/logo-kbkm-blue.png') }}"
-                                    width="64" alt="" style="margin-right: 5px" /></a>
-
-                            <a href="index.html"><img src="{{ asset('/assets/img/logo/logo-kbkm-blue.png') }}"
-                                    width="64" alt="" style="margin-right: 5px" /></a>
-
-                            <a href="index.html"><img src="{{ asset('/assets/img/logo/logo-kbkm-blue.png') }}"
-                                    width="64" alt="" style="margin-right: 5px" /></a>
-                        </div> --}}
-
                         <div class="main-menu d-none d-lg-block">
                             <nav>
                                 <ul id="navigation">
                                     <li><a href="{{ route('home') }}">Beranda</a></li>
                                     <li><a href="{{ route('products.index') }}">Produk Budaya</a></li>
                                     <li><a href="{{ route('travels.index') }}">Travel Budaya</a></li>
-                                    <li><a href="{{ route('contact') }}">Kontak</a></li>
+                                    {{-- <li><a href="{{ route('contact') }}">Kontak</a></li> --}}
                                 </ul>
                             </nav>
                         </div>
                     </div>
-                    {{-- <div class="header-right1 d-flex align-items-center">
-                        <div class="search">
-                            <ul class="d-flex align-items-center">
-                                <li>
-                                    <form action="#" class="form-box f-right">
-                                        <input type="text" name="Search" placeholder="Search" />
-
-                                    </form>
-                                </li>
-                                <li>
-                                    <a href="login.html" class="account-btn" target="_blank">Login</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div> --}}
-
                     <div class="col-12">
                         <div class="mobile_menu d-block d-lg-none"></div>
                     </div>
